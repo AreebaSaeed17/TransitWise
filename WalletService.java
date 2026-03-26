444ab109f4accc72ea4ba854cec86aea34433396
Areeba <asaeed.bese25seecs@seecs.edu.pk>
2026-03-27 01:12:00 +0500

    methods for user wallet added

--- WalletService.java
+++ WalletService.java
@@ -7,17 +7,27 @@ public class WalletService {
     // it checks and then assigns that money to that user's attribute of balance
     // to do this, user object is passed
     public boolean depositMoney(User user, double amount) {
-        if (amount <= 0) return false;
+        if (amount <= 0){
+             return false;}
+
         user.setWalletBalance(user.getWalletBalance() + amount);
         return true;
     }
-
-    public boolean deduct(User user, double amount) {
-        if (user.getWalletBalance() < amount) return false; // insufficient balance
-        user.setWalletBalance(user.getWalletBalance() - amount);
-        return true;
+    // this method deducts money from the wallet when user buys ticket
+    public boolean deductMoney(User user, double amount) {
+        // if balance is insufficient transaction fails.
+        if (user.getWalletBalance() < amount) {
+            System.out.println("Cant perform transaction!\n You hve insufficient balance in your wallet.");
+            return false;} 
+        else{
+            // deducting amount from balance
+            // first get the balance from user directly and then subtract that amount
+            // now the subtracted amount gets assigned to the user's account balance using set methof
+            user.setWalletBalance(user.getWalletBalance() - amount);
+            // as it is a boolean function so it has to return a boolean
+            return true;}
     }
-
+    // getBalance to now print the new balance of user wallet
     public double getBalance(User user) {
         return user.getWalletBalance();
     }
